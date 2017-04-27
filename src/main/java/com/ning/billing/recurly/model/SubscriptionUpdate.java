a98c08e9843c6534d8c394c079caf047d9aa3764
Farza <farza.pulikkalakath@tp-link.com>
2017-04-27 15:02:07 -0700

    Added netTerms, poNumber, couponCode, revenueScheduleType and remainingBillingCycles to SubscriptionUpdate

--- src/main/java/com/ning/billing/recurly/model/SubscriptionUpdate.java
+++ src/main/java/com/ning/billing/recurly/model/SubscriptionUpdate.java
@@ -17,14 +17,25 @@
 
 package com.ning.billing.recurly.model;
 
-import javax.xml.bind.annotation.XmlElement;
 import com.google.common.base.Objects;
+
+import javax.xml.bind.annotation.XmlElement;
 
 /**
  * Subscription object for update calls.
  * <p>
  * The timeframe parameter is specific to the update.
  */
+
+/*
+newly added fields:
+    netTerms
+    poNumber
+    couponCode
+    revenueScheduleType
+    remainingBillingCycles
+ */
+
 public class SubscriptionUpdate extends AbstractSubscription {
 
     public static enum Timeframe {
@@ -43,6 +54,7 @@ public class SubscriptionUpdate extends AbstractSubscription {
         this.timeframe = timeframe;
     }
 
+
     @XmlElement(name = "collection_method")
     private String collectionMethod;
 
@@ -52,6 +64,69 @@ public class SubscriptionUpdate extends AbstractSubscription {
 
     public void setCollectionMethod(Object collectionMethod) {
         this.collectionMethod = stringOrNull(collectionMethod);
+    }
+
+
+
+    @XmlElement(name = "net_terms")
+    private Integer netTerms;
+
+    public Integer getNetTerms() {
+        return netTerms;
+    }
+
+    public void setNetTerms(Integer netTerms) {
+        this.netTerms = netTerms;
+    }
+
+
+    @XmlElement(name = "po_number")
+    private String poNumber;
+
+    public String getPoNumber() {
+        return poNumber;
+    }
+
+    public void setPoNumber(String poNumber) {
+        this.poNumber = poNumber;
+    }
+
+
+
+    @XmlElement(name = "coupon_code")
+    private String couponCode;
+
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public void setCouponCode(String couponCode) {
+        this.couponCode = couponCode;
+    }
+
+
+
+    @XmlElement(name = "revenue_schedule_type")
+    private RevenueScheduleType revenueScheduleType;
+
+    public RevenueScheduleType getRevenueScheduleType() {
+        return revenueScheduleType;
+    }
+
+    public void setRevenueScheduleType(RevenueScheduleType revenueScheduleType) {
+        this.revenueScheduleType = revenueScheduleType;
+    }
+
+
+    @XmlElement(name = "remaining_billing_cycles")
+    private Integer remainingBillingCycles;
+
+    public Integer getRemainingBillingCycles() {
+        return remainingBillingCycles;
+    }
+
+    public void setRemainingBillingCycles(Integer remainingBillingCycles) {
+        this.remainingBillingCycles = remainingBillingCycles;
     }
 
     @Override
@@ -68,6 +143,26 @@ public class SubscriptionUpdate extends AbstractSubscription {
             return false;
         }
 
+        //newly added
+        if (netTerms != null ? !netTerms.equals(that.netTerms) : that.netTerms != null) {
+            return false;
+        }
+        if (collectionMethod != null ? !collectionMethod.equals(that.collectionMethod) : that.collectionMethod != null) {
+            return false;
+        }
+        if (poNumber != null ? !poNumber.equals(that.poNumber) : that.poNumber != null) {
+            return false;
+        }
+        if (couponCode != null ? !couponCode.equals(that.couponCode) : that.couponCode != null) {
+            return false;
+        }
+        if (revenueScheduleType != null ? !revenueScheduleType.equals(that.revenueScheduleType) : that.revenueScheduleType != null) {
+            return false;
+        }
+        if (remainingBillingCycles != null ? !remainingBillingCycles.equals(that.remainingBillingCycles) : that.remainingBillingCycles != null) {
+            return false;
+        }
+
         return true;
     }
 
@@ -75,7 +170,14 @@ public class SubscriptionUpdate extends AbstractSubscription {
     public int hashCode() {
         return Objects.hashCode(
                 timeframe,
-                collectionMethod
+                collectionMethod,
+
+                //newly added
+                netTerms,
+                poNumber,
+                couponCode,
+                revenueScheduleType,
+                remainingBillingCycles
         );
     }
 }
